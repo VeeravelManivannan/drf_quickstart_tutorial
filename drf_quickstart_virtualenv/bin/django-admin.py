3d8e19bf4d5650e3d42679d070c5879569982d19
Veeravel Manivannan <veeravelpro@gmail.com>
2019-07-30 07:10:25 +0200

    django restframework quick start guide

---
+++ drf_quickstart_virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mannivav/GitRepositories/drf_quickstart_tutorial/drf_quickstart_virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ drftutorial/drfquickstartapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DrfquickstartappConfig(AppConfig):
+    name = 'drfquickstartapp'
